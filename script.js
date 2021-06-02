cfeb94a3651c24e3cf165ba5b44fdd9880a236c4
tmannazy <e.anene@hotmail.com>
2021-06-02 22:16:26 +0100

    Create displayController for players and a gameboard

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const gameBoard = (() => {
-    const gameObj = { gameBoardArr: ['x', 'o', 'x', 'o', 'x', 'o'] };
-    return { gameObj };
-});
-
-const displayController = (() => {
-
-});
+    const gameObj = {
+        gameBoardArr: ['x', 'o', 'x', 'o', 'x', 'o'],
+        getDOM() {
+            const getSquares = document.getElementsByClassName('box');
+            const squares = () => {
+                getSquaresArr = Array.from(getSquares);
+                getSquaresArr.forEach((item, index) => item.appendChild(this.gameBoardArr[index]));
+            }
+            getSquares.addEventListener('click', squares);
+        }
+    };
+    return gameObj;
+})();
 
 const Player = (playerName) => {
     return { playerName };
 };
 
+const displayController = (() => {
+    const playerOne = Player('Nevis');
+    const playerTwo = Player('Tosman');
+    return Object.assign({}, playerOne, playerTwo);
+})();
 
-const playerOne = Player('Nevis');
-const playerTwo = Player('Tosman');
-const gameFlow = {};
+
 
