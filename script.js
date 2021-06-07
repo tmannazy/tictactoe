a84149b83fb7c58aec44bbabc5bfa5c2eadd8ffd
tmannazy <e.anene@hotmail.com>
2021-06-07 23:53:45 +0100

    Change methods location to sit where appropriate

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 const gameBoard = (() => {
-    const gameObj = {
-        gameBoardArr: ['x', 'o'],
-    };
-    return gameObj;
+    const gameBoardArr = ['x', 'o'];
+    return { gameBoardArr };
 })();
 
 const displayController = (() => {
-    const playerOne = () => emma.cacheMarkDOM();
-    const playerTwo = () => nev.cacheMarkDOM();
-    return {
-        playerOne,
-        playerTwo
-    };
-})();
-
-const Player = (playerName, playerLetter) => {
-    // const player = playerName;
+    // const playerOne = () => emma.cacheMarkDOM();
+    // const playerTwo = () => nev.cacheMarkDOM();
 
     // cache DOM
     const getSquares = document.querySelector('.game-board');
-    const getMarkSelect = document.querySelector('.marks');
 
     const squares = e => {
         if (e.target !== e.currentTarget && e.target.textContent == '') {
             // const divIndex = Array.from(e.target.parentElement.children).indexOf(e.target);
             const el = gameBoard.gameBoardArr.forEach(item => {
                 if (item === playerLetter) {
-                    e.target.textContent = `${item}`;
+                    e.target.textContent = item;
                 }
             });
         } e.stopPropagation();
@@ -36,18 +25,28 @@ const Player = (playerName, playerLetter) => {
         getSquares.addEventListener('click', squares);
     };
 
+    return {
+        cacheBoardDOM
+    };
+
+})();
+
+const Player = (playerName, playerLetter) => {
+    const getMarkSelect = document.querySelector('.marks');
+
     const cacheMarkDOM = () => {
         getMarkSelect.addEventListener('click', e => {
             if (e.target !== e.currentTarget && e.target.textContent.toLowerCase() === playerLetter) {
-                cacheBoardDOM();
+                displayController.cacheBoardDOM();
             } e.stopPropagation();
         });
     }
+
     return { cacheMarkDOM };
 };
 
 const emma = Player('Emmanuel', 'x');
 const nev = Player('Nevis', 'o');
 
-displayController.playerOne();
-displayController.playerTwo();
+emma.cacheMarkDOM();
+nev.cacheMarkDOM();
