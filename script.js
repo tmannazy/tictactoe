d20703cd42e729b2a8da9937529b5ba61e6c22ab
tmannazy <e.anene@hotmail.com>
2021-06-09 01:08:56 +0100

    Change where the game logics reside

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
+const Player = (playerName, playerMark) => {
+    return { playerName, playerMark };
+};
+
+const emma = Player('Emmanuel', 'x');
+const nev = Player('Nevis', 'o');
+
+
 const gameBoard = (() => {
     const gameBoardArr = ['x', 'o'];
-    return { gameBoardArr };
-})();
-
-const displayController = (() => {
-    // const playerOne = () => emma.cacheMarkDOM();
-    // const playerTwo = () => nev.cacheMarkDOM();
 
     // cache DOM
+    const getMarkSelect = document.querySelector('.marks');
     const getSquares = document.querySelector('.game-board');
+
+    const isPlayerChoice = (firstPlayerLetter, secondPlayerLetter) => {
+        getMarkSelect.addEventListener('click', e => {
+            if (e.target !== e.currentTarget) {
+                if (e.target.textContent.toLowerCase() === firstPlayerLetter) {
+                    cacheBoardDOM(firstPlayerLetter);
+                } else if (e.target.textContent.toLowerCase() === secondPlayerLetter) {
+                    cacheBoardDOM(secondPlayerLetter);
+                }
+            } e.stopPropagation();
+        });
+    }
 
     const squares = e => {
         if (e.target !== e.currentTarget && e.target.textContent == '') {
             // const divIndex = Array.from(e.target.parentElement.children).indexOf(e.target);
-            const el = gameBoard.gameBoardArr.forEach(item => {
-                if (item === playerLetter) {
+            const el = gameBoardArr.forEach(item => {
+                if (item === cacheBoardDOM(item)) {
+                    e.target.textContent = item;
+                } else {
                     e.target.textContent = item;
                 }
             });
         } e.stopPropagation();
     }
 
-    const cacheBoardDOM = () => {
+    const cacheBoardDOM = (playerLetter) => {
         getSquares.addEventListener('click', squares);
     };
 
-    return {
-        cacheBoardDOM
-    };
-
+    return { isPlayerChoice };
 })();
 
-const Player = (playerName, playerLetter) => {
-    const getMarkSelect = document.querySelector('.marks');
+const displayController = (() => {
+    const players = () => {
+        gameBoard.isPlayerChoice(emma.playerMark, nev.playerMark);
+    };
 
-    const cacheMarkDOM = () => {
-        getMarkSelect.addEventListener('click', e => {
-            if (e.target !== e.currentTarget && e.target.textContent.toLowerCase() === playerLetter) {
-                displayController.cacheBoardDOM();
-            } e.stopPropagation();
-        });
-    }
+    return {
+        players
+    };
+})();
 
-    return { cacheMarkDOM };
-};
 
-const emma = Player('Emmanuel', 'x');
-const nev = Player('Nevis', 'o');
-
-emma.cacheMarkDOM();
-nev.cacheMarkDOM();
+displayController.players();
