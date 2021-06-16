d109c1d8a55294674ce926f9804ef46445155cf9
tmannazy <e.anene@hotmail.com>
2021-06-16 03:29:07 +0100

    Create a variable to store user's letter select

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ const nev = Player('Nevis', 'o');
 
 const gameBoard = (() => {
     const gameBoardArr = ['x', 'o'];
-    const storePlayerOneLetter = [];
-    const storePlayerTwoLetter = [];
-    const getPlayerOne = () => emma;
-    const getPlayerTwo = () => nev;
+    // const storePlayerOneLetter = [];
+    // const storePlayerTwoLetter = [];
+    const getPlayerOne = emma;
+    const getPlayerTwo = nev;
+    let val;
+
 
 
     // cacheDOM
@@ -19,20 +21,19 @@ const gameBoard = (() => {
 
 
     const currentPlayer = player => {
-        if (player === getPlayerOne().playerMark) {
-            displayController.outputMark();
-        } else if (player === getPlayerTwo().playerMark) {
-            displayController.outputMark();
+        if (player === getPlayerOne.playerMark) {
+            val = player;
+        } else if (player === getPlayerTwo.playerMark) {
+            val = player;
         }
-    }
+        return val;
+    };
 
     const getLetterChoice = e => {
         if (e.target.closest('button')) {
-            if (e.target.textContent.toLowerCase() === getPlayerOne().playerMark) {
-                storePlayerOneLetter.push(e.target.textContent.toLowerCase());
+            if (e.target.textContent.toLowerCase() === getPlayerOne.playerMark) {
                 currentPlayer(e.target.textContent.toLowerCase());
-            } else if (e.target.textContent.toLowerCase() === getPlayerTwo().playerMark) {
-                storePlayerTwoLetter.push(e.target.textContent.toLowerCase());
+            } else if (e.target.textContent.toLowerCase() === getPlayerTwo.playerMark) {
                 currentPlayer(e.target.textContent.toLowerCase());
             }
         }
@@ -44,9 +45,9 @@ const gameBoard = (() => {
 
 
     return {
-        getPlayerOne,
-        getPlayerTwo,
-        // storeUserSelect,
+        currentPlayer,
+        // storePlayerOneLetter,
+        // storePlayerTwoLetter,
         gameBoardArr,
     };
 })();
@@ -61,24 +62,53 @@ const displayController = (() => {
 
     const displayMark = e => {
         if (e.target.closest('div.box')) {
+            const playerLet = gameBoard.currentPlayer();
             const el = gameBoard.gameBoardArr.some(item => {
-                if (item === gameBoard.storeUserSelect[0]) {
-                    e.target.textContent = item;
+                if (item === playerLet) {
+                    if (playerLet === 'x') {
+                        e.target.textContent = item;
+                    }
+                    gameBoard.currentPlayer('o');
                 }
-                if (item !== gameBoard.storeUserSelect[0]) {
-                    e.target.textContent = item;
+                else if (item === playerLet) {
+                    if (playerLet === 'o') {
+                        e.target.textContent = item;
+                    }
+                    gameBoard.currentPlayer('x');
                 }
             });
+            //             gameBoard.currentPlayer('x');
+            //         });
+            // } const el = gameBoard.gameBoardArr.some(item => {
+            //     if (item === 'x') {
+            //         e.target.textContent = item.target.textContent = item;
+            //     }
+            // });
+            // gameBoard.currentPlayer('o');
+            // //
+            // if (item !== gameBoard.storeUserSelect[0]) {
+            //     e.target.textContent = item;
+            // }
+
+            //     switch (item) {
+            //         case 'gameBoard.currentPlayer':
+            //             e.target.textContent = item;
+            //             break;
+
+            //     }
+            // });
         }
     };
 
     // bindEvents
-    const outputMark = () => { getBoardContainer.addEventListener('click', displayMark.bind(null, displayMark)); };
+    // const outputMark = () => {
+    getBoardContainer.addEventListener('click', displayMark);
+    // };
 
 
 
     return {
-        outputMark
+        displayMark
     };
 
 })();
